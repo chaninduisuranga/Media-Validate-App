cc5bbf0f07e359377904720203347415dd835add
chaninduisuranga <159243993+chaninduisuranga@users.noreply.github.com>
2026-06-02 09:33:42 +0530

    fix(docker): add non-root user to comply with choreo security policies

--- docker/Dockerfile.go
+++ docker/Dockerfile.go
@@ -16,10 +16,15 @@ RUN go build -o main .
 # Run stage
 FROM alpine:latest
 
-WORKDIR /root/
+WORKDIR /app
 
 # Copy the binary from the build stage
 COPY --from=build /app/main .
+
+# Add non-root user required by WSO2 Choreo
+RUN addgroup -S choreouser && adduser -S choreouser -G choreouser -u 10014
+RUN chown -R choreouser:choreouser /app
+USER 10014
 
 # Expose the API port
 EXPOSE 8081
